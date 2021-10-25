5fc317762b0ebf4f9a90c9aadabd7561a0223a4b
alokp10 <pathakalok36@gmail.com>
2021-10-25 22:26:46 +0530

    Updated Controller

--- src/main/java/com/alok/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/alok/springbootdemo/SpringBootDemoApplication.java
@@ -12,7 +12,7 @@ public class SpringBootDemoApplication {
 	@RequestMapping("/")
 	public String message()
 	{
-		return "Hello World";
+		return "Hello World!!!";
 	}
 
 	public static void main(String[] args) {
